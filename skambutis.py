14cfc877b1272d2928af12b613d6433a07fda789
Mikas1526 <mikas.alisauskas@gmail.com>
2025-01-22 14:08:41 +0200

    modified:   skambuciuLaikai.txt 	modified:   skambutis.py

--- skambutis.py
+++ skambutis.py
@@ -4,63 +4,83 @@ from playsound import playsound
 def setTimes(adress: str) -> list[dict]:
     """ insert txt document adress in order to load times """
     times = []
-    time = {'h': 0, 'min': 0}
+    time = {'h': 0, 'min': 0, 'type': 1}
     doc = open(adress)
     for line in doc.readlines():
-        line = line.strip().split(':')
+        line = line.strip().split()
+        time['type'] = int(line[0])
+        line = line[1].split(':')
         time['h'], time['min'] = int(line[0]), int(line[1])
         times.append(time.copy())
     doc.close()
     return times
 
-def skambutis():
-    playsound('drums.wav')
+def skambutis(bellType: int | None = 1):
+    """ play sound """
+    match bellType:
+        case 1:
+            playsound(firstBellFile)
+        case 2:
+            playsound(secondBellFile)
+        case 3:
+            playsound(finalBellFile)
+        case _:
+            playsound(firstBellFile)
+        
+            
 
-status = {'on': 'aktyvus', 'off': 'neaktyvus'}
-timeSettings = 'skambuciuLaikai.txt'
+# Global variables
+isBellActive = False
+timeSettingsFile = 'skambuciuLaikai.txt'
+firstBellFile = 'skambutis1.mp3'
+secondBellFile = 'skambutis2.mp3'
+finalBellFile = 'skambutis2.mp3'
 times = []
 
+# Start of GUI
 m = Tk()
-m.geometry("300x250")
+m.geometry("450x350")
 m.title('Mokyklos skambutis')
 
+txtNustatymai = Label(m, text="Nustatymai")
+txtNustatymai.grid(row=0, columnspan=3)
 
-Label(m, text='Nustatymai dokumente:').grid(row=0)
+Label(m, text='Nustatymai dokumente:').grid(row=1)
 
 entrSetDoc = Entry(m)
-entrSetDoc.insert(0, timeSettings)
-entrSetDoc.grid(row=0, column=2)
+entrSetDoc.insert(0, timeSettingsFile)
+entrSetDoc.grid(row=1, column=2)
 
-Label(m, text='Įspėjimo skambutis:').grid(row=1)
+Label(m, text='Įspėjimo skambutis:').grid(row=2)
 
 firstBell = Entry(m)
-firstBell.insert(0, timeSettings)
-firstBell.grid(row=1, column=2)
+firstBell.insert(0, firstBellFile)
+firstBell.grid(row=2, column=2)
 
-Label(m, text='Pradžios skambutis:').grid(row=2)
+Label(m, text='Pradžios skambutis:').grid(row=3)
 
 secondBell = Entry(m)
-secondBell.insert(0, timeSettings)
-secondBell.grid(row=2, column=2)
+secondBell.insert(0, secondBellFile)
+secondBell.grid(row=3, column=2)
 
-Label(m, text='Pabaigos skambutis:').grid(row=3)
+Label(m, text='Pabaigos skambutis:').grid(row=4)
 
 finalBell = Entry(m)
-finalBell.insert(0, timeSettings)
-finalBell.grid(row=3, column=2)
+finalBell.insert(0, finalBellFile)
+finalBell.grid(row=4, column=2)
 
-txtAntraste = Label(m, text="Sistemoje įvesti laikai")
-txtAntraste.grid(row=4)
+txtAntraste = Label(m, text="Skambutis")
+txtAntraste.grid(row=5, columnspan=3)
 
 scrlTimeList = Scrollbar(m)
-scrlTimeList.grid(row=4, column=1)
+scrlTimeList.grid(row=6, column=1)
 lstBoxTimes = Listbox(m, yscrollcommand=scrlTimeList.set)
-times = setTimes(timeSettings)
+times = setTimes(timeSettingsFile)
 for t in times:
-    lstBoxTimes.insert("end", f'{t["h"]}.{t["min"]}')
-lstBoxTimes.grid(row=4, column=0)
+    lstBoxTimes.insert("end", f'{t["type"]} {t["h"]:02d}.{t["min"]:02d}')
+lstBoxTimes.grid(row=6, column=0)
 scrlTimeList.config(command=lstBoxTimes.yview)
 
-Button(m, text="Testas", command=skambutis).grid(row=5)
+Button(m, text="Testas", command=lambda: skambutis(2)).grid(row=7)
 
 m.mainloop()
